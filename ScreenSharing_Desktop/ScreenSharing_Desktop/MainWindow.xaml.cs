3ae05077932c969f0b81b29b17aa5210bee69197
doesluck1026 <58068335+doesluck1026@users.noreply.github.com>
2021-01-18 22:13:00 +0300

    up

--- ScreenSharing_Desktop/ScreenSharing_Desktop/MainWindow.xaml.cs
+++ ScreenSharing_Desktop/ScreenSharing_Desktop/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace ScreenSharing_Desktop
@@ -27,7 +33,132 @@ namespace ScreenSharing_Desktop
         {
             InitializeComponent();
         }
+        static string NetworkGateway()
+        {
+            string ip = null;
 
+            foreach (NetworkInterface f in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (f.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (GatewayIPAddressInformation d in f.GetIPProperties().GatewayAddresses)
+                    {
+                        ip = d.Address.ToString();
+                    }
+                }
+            }
+
+            return ip;
+        }
+        public void Ping_all()
+        {
+
+            string gate_ip = NetworkGateway();
+
+            //Extracting and pinging all other ip's.
+            string[] array = gate_ip.Split('.');
+
+            for (int i = 2; i <= 255; i++)
+            {
+
+                string ping_var = array[0] + "." + array[1] + "." + array[2] + "." + i;
+
+                //time in milliseconds           
+                Ping(ping_var, 8, 4000);
+
+            }
+
+        }
+
+        public void Ping(string host, int attempts, int timeout)
+        {
+            for (int i = 0; i < attempts; i++)
+            {
+                new Thread(delegate ()
+                {
+                    try
+                    {
+                        System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
+                        ping.PingCompleted += new PingCompletedEventHandler(PingCompleted);
+                        ping.SendAsync(host, timeout, host);
+                    }
+                    catch
+                    {
+                        // Do nothing and let it try again until the attempts are exausted.
+                        // Exceptions are thrown for normal ping failurs like address lookup
+                        // failed.  For this reason we are supressing errors.
+                    }
+                }).Start();
+            }
+        }
+        List<string> list = new List<string>();
+        private void PingCompleted(object sender, PingCompletedEventArgs e)
+        {
+            string ip = (string)e.UserState;
+            if (e.Reply != null && e.Reply.Status == IPStatus.Success)
+            {
+                string hostname = GetHostName(ip);
+                string macaddres = GetMacAddress(ip);
+                string[] arr = new string[3];
+
+                //store all three parameters to be shown on ListView
+                arr[0] = ip;
+                arr[1] = hostname;
+                arr[2] = macaddres;
+                if(!list.Contains(arr[0]))
+                        list.Add(arr[0]);
+            }
+            else
+            {
+                // MessageBox.Show(e.Reply.Status.ToString());
+            }
+        }
+        public string GetHostName(string ipAddress)
+        {
+            try
+            {
+                IPHostEntry entry = Dns.GetHostEntry(ipAddress);
+                if (entry != null)
+                {
+                    return entry.HostName;
+                }
+            }
+            catch (SocketException)
+            {
+                // MessageBox.Show(e.Message.ToString());
+            }
+
+            return null;
+        }
+
+
+        //Get MAC address
+        public string GetMacAddress(string ipAddress)
+        {
+            string macAddress = string.Empty;
+            System.Diagnostics.Process Process = new System.Diagnostics.Process();
+            Process.StartInfo.FileName = "arp";
+            Process.StartInfo.Arguments = "-a " + ipAddress;
+            Process.StartInfo.UseShellExecute = false;
+            Process.StartInfo.RedirectStandardOutput = true;
+            Process.StartInfo.CreateNoWindow = true;
+            Process.Start();
+            string strOutput = Process.StandardOutput.ReadToEnd();
+            string[] substrings = strOutput.Split('-');
+            if (substrings.Length >= 8)
+            {
+                macAddress = substrings[3].Substring(Math.Max(0, substrings[3].Length - 2))
+                         + "-" + substrings[4] + "-" + substrings[5] + "-" + substrings[6]
+                         + "-" + substrings[7] + "-"
+                         + substrings[8].Substring(0, 2);
+                return macAddress;
+            }
+
+            else
+            {
+                return "OWN Machine";
+            }
+        }
         private void btn_Share_Click(object sender, RoutedEventArgs e)
         {
             Reset();
@@ -115,6 +246,10 @@ namespace ScreenSharing_Desktop
         }
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Debug.WriteLine("item : " + i + " : " + list[i]);
+            }
             CloseApp();
         }
 
@@ -139,6 +274,8 @@ namespace ScreenSharing_Desktop
                     chc_AutoShare.IsChecked = IsAutoShareEnabled;
                 });
             }
+            Ping_all();
+           
         }
 
         private void chc_AutoShare_Click(object sender, RoutedEventArgs e)
