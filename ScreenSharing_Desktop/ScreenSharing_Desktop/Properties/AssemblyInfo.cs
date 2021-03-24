57529ef2ba991741922600ff6c43f3870402ff9e
doesluck1026 <58068335+doesluck1026@users.noreply.github.com>
2021-03-24 15:37:06 +0300

    update

--- ScreenSharing_Desktop/ScreenSharing_Desktop/Properties/AssemblyInfo.cs
+++ ScreenSharing_Desktop/ScreenSharing_Desktop/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScreenSharing_Desktop")]
+[assembly: AssemblyTitle("Screen Sharing App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5")]
-[assembly: AssemblyFileVersion("1.0.5")]
+[assembly: AssemblyVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6")]
